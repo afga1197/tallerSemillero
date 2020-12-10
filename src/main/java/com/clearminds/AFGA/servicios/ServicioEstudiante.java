7c92c528a1cd51b4d2b3072485a9b0a1a2be724c
Andres Gomez <andrefelgoal@gmail.com>
2020-12-10 10:14:59 -0500

    Modificacion de ingresar datos

--- src/main/java/com/clearminds/AFGA/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/AFGA/servicios/ServicioEstudiante.java
@@ -1,9 +1,7 @@
 package com.clearminds.AFGA.servicios;
 
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import com.clearminds.AFGA.dtos.Estudiante;
 import com.clearminds.AFGA.excepciones.BDDException;
 
@@ -13,10 +11,16 @@ public class ServicioEstudiante extends ServicioBase {
 		abrirConexion();
 		System.out.println("insertando estudiante " + estudiante.toString());
 		Statement stmt = null;
+		String sql = "";
+		if(estudiante.getEdad()==null){	
+			sql = "INSERT INTO estudiantes (nombre, apellido) VALUES('" + estudiante.getNombre() + "','"
+					+ estudiante.getApellido() + "')";
+		} else{
+			sql = "INSERT INTO estudiantes (nombre, apellido, edad) VALUES('" + estudiante.getNombre() + "','"
+					+ estudiante.getApellido()+ "','" +estudiante.getEdad() + "')";
+		}
 		try {
 			stmt = conexion.createStatement();
-			String sql = "INSERT INTO estudiantes (nombre, apellido) VALUES('" + estudiante.getNombre() + "','"
-					+ estudiante.getApellido() + "')";
 			System.out.println(sql);
 			stmt.executeUpdate(sql);
 		} catch (SQLException e) {
@@ -31,9 +35,14 @@ public class ServicioEstudiante extends ServicioBase {
 		abrirConexion();
 		System.out.println("actualizando estudiante " + estudiante.toString());
 		Statement stmt = null;
+		String sql = "";
+		if(estudiante.getEdad()==null){	
+			sql = "UPDATE estudiantes SET nombre='"+estudiante.getNombre()+"', apellido='"+estudiante.getApellido()+"' WHERE id='"+estudiante.getId()+"'";
+		} else{
+			sql = "UPDATE estudiantes SET nombre='"+estudiante.getNombre()+"', apellido='"+estudiante.getApellido()+"', edad='"+estudiante.getEdad()+"' WHERE id='"+estudiante.getId()+"'";
+		}
 		try {
 			stmt = conexion.createStatement();
-			String sql = "UPDATE estudiantes SET nombre='"+estudiante.getNombre()+"', apellido='"+estudiante.getApellido()+"' WHERE id='"+estudiante.getId()+"'";
 			System.out.println(sql);
 			stmt.executeUpdate(sql);
 		} catch (SQLException e) {
